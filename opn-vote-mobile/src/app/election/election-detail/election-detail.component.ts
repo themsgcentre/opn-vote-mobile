5f5a18726475345f30b0fff93a86d72fc8c6b1a8
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-01-12 17:27:46 +0100

    added more info

--- opn-vote-mobile/src/app/election/election-detail/election-detail.component.ts
+++ opn-vote-mobile/src/app/election/election-detail/election-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ElectionImageComponent } from 'src/app/home-page/election-image/election-image.component';
 import { ElectionDTO } from 'src/app/interfaces/election-dto';
+import { daysBetween } from 'src/app/operations/date-operations';
 
 @Component({
   selector: 'app-election-detail',
@@ -8,6 +9,14 @@ import { ElectionDTO } from 'src/app/interfaces/election-dto';
   styleUrls: ['./election-detail.component.scss'],
   imports: [ElectionImageComponent]
 })
-export class ElectionDetailComponent {
+export class ElectionDetailComponent implements OnChanges{
   @Input() election: ElectionDTO | undefined;
+  daysTilEnd: number = 0;
+
+  ngOnChanges(): void {
+    if(this.election && this.election.endDate) {
+      const now = new Date();
+      this.daysTilEnd = daysBetween(now, this.election.endDate)
+    }
+  }
 }
--- opn-vote-mobile/src/app/home-page/election-overview/election-overview.component.ts
+++ opn-vote-mobile/src/app/home-page/election-overview/election-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges } from '@angular/core';
 import { ElectionDTO } from 'src/app/interfaces/election-dto';
 import { ElectionInfoComponent } from "../election-info/election-info.component";
 import { ElectionImageComponent } from "../election-image/election-image.component";
+import { daysBetween } from 'src/app/operations/date-operations';
 
 @Component({
   selector: 'app-election-overview',
@@ -16,13 +17,7 @@ export class ElectionOverviewComponent implements OnChanges {
   ngOnChanges(): void {
     const now = new Date();
     if(this.election && this.election.endDate) {
-      this.daysTilEnd = this.daysBetween(now, this.election.endDate)
+      this.daysTilEnd = daysBetween(now, this.election.endDate)
     }
-  }
-
-  private daysBetween(date1: Date, date2: Date): number {
-    const msPerDay = 1000 * 60 * 60 * 24;
-    const diffMs = date2.getTime() - date1.getTime();
-    return Math.floor(diffMs / msPerDay);
   }
 }
