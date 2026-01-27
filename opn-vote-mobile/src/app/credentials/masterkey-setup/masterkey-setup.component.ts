847f4d7745f832ae959ebac1e9116381a51cae7b
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-01-27 14:57:06 +0100

    added top bar component

--- opn-vote-mobile/src/app/credentials/masterkey-setup/masterkey-setup.component.ts
+++ opn-vote-mobile/src/app/credentials/masterkey-setup/masterkey-setup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-masterkey-setup',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterkeySetupComponent  implements OnInit {
 
+  @Input() canSkip: boolean = false;
   constructor() { }
 
   ngOnInit() {}
