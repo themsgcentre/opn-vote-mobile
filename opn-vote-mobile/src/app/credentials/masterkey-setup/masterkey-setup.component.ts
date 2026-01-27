60ad000ea227e0d751bb4861b9b4862c9d2e46da
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-01-27 15:00:40 +0100

    skip button

--- opn-vote-mobile/src/app/credentials/masterkey-setup/masterkey-setup.component.ts
+++ opn-vote-mobile/src/app/credentials/masterkey-setup/masterkey-setup.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MasterkeySetupComponent  implements OnInit {
 
   @Input() canSkip: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {}
