e954c74bf683e58da71c399bdcb8000433e47f2a
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-03-06 16:16:37 +0100

    url path in constants and log for data check

--- opn-vote-mobile/src/app/apollo.config.ts
+++ opn-vote-mobile/src/app/apollo.config.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
+import { UrlPaths } from './globals/url-paths';
 
 export const graphClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://graphql.dev.opn.vote/subgraphs/name/opnvote-dev-0x6df22e4e4ede7e4e73b3608bcb5508b892a1fd28/',
+    uri: UrlPaths.graphUrl,
   }),
   cache: new InMemoryCache(),
 });
