8eb7b2e4641f0af542708e2c0aaf63a73878a17e
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-03-12 12:38:13 +0100

    loaded election into voting component

--- opn-vote-mobile/src/app/election/voting/voting.component.ts
+++ opn-vote-mobile/src/app/election/voting/voting.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Election } from 'src/app/interfaces/election';
+import { BallotService } from 'src/app/services/ballot-service';
+import { ElectionService } from 'src/app/services/election-service';
 
 @Component({
   selector: 'app-voting',
@@ -7,8 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotingComponent  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private electionService: ElectionService,
+    private ballotService: BallotService,
+  ) { }
 
-  ngOnInit() {}
+  election: Observable<Election | null> = new Observable();
+  error: string | null = null;
+
+  ngOnInit() {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    const electionId = idParam ? Number(idParam) : NaN;
+    console.log("VotingComponent initialized with electionId", electionId);
+
+    if(!isNaN(electionId)) {
+      console.log("Loading election with id", electionId);
+      this.election = this.electionService.getElectionById(electionId)
+      this.election.subscribe(e => {
+        console.log(e?.questions);
+      })
+    }
+    else {
+      this.error = "Ungültige Wahl-ID";
+    }
+  }
 
 }
