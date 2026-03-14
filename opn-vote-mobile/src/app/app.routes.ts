f82609532652c01ec77da6290509fa3f66954ebb
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-03-14 16:35:57 +0100

    fixed rebase issues

--- opn-vote-mobile/src/app/app.routes.ts
+++ opn-vote-mobile/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { VoteHistoryComponent } from './vote-history/vote-history.component';
 import { ElectionDetailViewComponent } from './election/election-detail-view/election-detail-view.component';
 import { VotingComponent } from './election/voting/voting.component';
 import { RegistrationComponent } from './election/registration/registration.component';
-import { MasterKeySetupComponent } from './credentials/master-key-setup/master-key-setup.component';
+import { MasterKeyManagementComponent } from './credentials/master-key-management/master-key-management.component';
 
 export const routes: Routes = [
   {
@@ -17,7 +17,7 @@ export const routes: Routes = [
   },
   {
     path: 'user',
-    component: MasterKeySetupComponent
+    component: MasterKeyManagementComponent
   },
   {
     path: '',
